954c2051a3aa66574b6555e09eb4b7aaa387011a
Vika Petrenko <vikamiranet@gmail.com>
2021-07-26 21:44:27 +0300

    feat(geo): get endpoint

--- nomoko-api/src/geo/geo.controller.ts
+++ nomoko-api/src/geo/geo.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { GeoService } from '@app/geo/geo.service';
+import {Buildings} from "@app/geo/geo.entity";
 
 @Controller('geo')
 export class GeoController {
@@ -8,5 +9,10 @@ export class GeoController {
   @Post('/')
   import(): Promise<any> {
     return this.service.import();
+  }
+
+  @Get('/')
+  getList(): Promise<Buildings[]> {
+    return this.service.find();
   }
 }
--- nomoko-api/src/geo/geo.service.ts
+++ nomoko-api/src/geo/geo.service.ts
@@ -7,7 +7,7 @@ import { Buildings } from '@app/geo/geo.entity';
 import { plainToClass } from 'class-transformer';
 import { BUILDING_TYPES } from '@app/geo/geo.constants';
 import { Point } from 'geojson';
-import {Repository} from "typeorm";
+import { Repository } from 'typeorm';
 
 const FILE = 'properties_f.csv';
 
@@ -20,7 +20,10 @@ interface GeoCsvRow {
 
 @Injectable()
 export class GeoService {
-  constructor(@InjectRepository(Buildings) private buildingsRepository: Repository<Buildings>,) {}
+  constructor(
+    @InjectRepository(Buildings)
+    private buildingsRepository: Repository<Buildings>,
+  ) {}
 
   async import(): Promise<{ response: string }> {
     return new Promise((resolve, reject) => {
@@ -44,10 +47,14 @@ export class GeoService {
           );
         })
         .on('data', (data: Buildings) => {
-            return this.buildingsRepository.save(data);
+          return this.buildingsRepository.save(data);
         })
         .on('error', reject)
         .on('end', resolve);
     });
+  }
+
+  async find(): Promise<Buildings[]> {
+    return this.buildingsRepository.find();
   }
 }
