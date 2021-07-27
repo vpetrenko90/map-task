d924f3280828f6ac74073724e60e8b0cf5102369
Vika Petrenko <vikamiranet@gmail.com>
2021-07-27 15:22:38 +0300

    feat(front): display popups

--- nomoko-front/src/app/components/App.tsx
+++ nomoko-front/src/app/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import L from 'leaflet';
 import {
   MapContainer,
@@ -11,22 +11,6 @@ import {
 import './App.css';
 import markerImage from './pin.svg';
 
-const position = [47.3810326, 8.5583433];
-
-const markers = [
-  [47.3375577, 8.5266948],
-  [47.3540935, 8.5553344],
-  [47.3550125, 8.603635],
-  [47.3550125, 8.603635],
-  [47.3550125, 8.603635],
-  [47.3550125, 8.603635],
-  [47.3576212, 8.5729142],
-  [47.3607583, 8.5521945],
-  [47.3633783, 8.5597119],
-  [47.3646544, 8.5105568],
-  [47.3676673, 8.5309105],
-];
-
 function MyComponent({ onSet }: { onSet: any }) {
   const map = useMapEvent('click', e => {
     onSet(e.latlng);
@@ -37,6 +21,7 @@ function MyComponent({ onSet }: { onSet: any }) {
 function App() {
   const [text, setText] = useState('');
   const [position, setPosition] = useState<any>(null);
+  const [markers, setMarkers] = useState<any>([]);
 
   const onSetHandle = async (e: any) => {
     const { lat, lng } = e;
@@ -44,7 +29,7 @@ function App() {
     console.log('ADD MARKER', e);
     //@ts-ignore
     const result = await getPrice({ lat, lng });
-    setText(`Estimated price = ${result?.price}`);
+    setText(`Estimated price = $${result?.price}`);
   };
 
   const markerIcon = new L.Icon({
@@ -54,6 +39,12 @@ function App() {
     popupAnchor: [1, -34],
   });
 
+  const getMarkers = async () => {
+    const response = await fetch(`http://localhost:2222/geo`);
+    const items = await response.json();
+    setMarkers(items);
+  };
+
   const getPrice = async (point: { lng: string; lat: string }) => {
     const { lng, lat } = point;
     const response = await fetch(
@@ -62,6 +53,10 @@ function App() {
     const items = await response.json();
     return items;
   };
+
+  useEffect(() => {
+    getMarkers();
+  }, []);
 
   return (
     <div className="App">
@@ -80,14 +75,15 @@ function App() {
             <Popup>{text}</Popup>
           </Marker>
         )}
-        {markers.length > 0 &&
-          markers.map(([long, lat]) => (
-            <Marker position={[long, lat]}>
-              <Popup>
-                {long} / {lat}
-              </Popup>
+        {markers.map(({ location, price }: any) => {
+          return (
+            <Marker
+              position={[location.coordinates[1], location.coordinates[0]]}
+            >
+              <Popup>${price}</Popup>
             </Marker>
-          ))}
+          );
+        })}
       </MapContainer>
     </div>
   );
